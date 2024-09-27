2af8e7536b4b7b667d9ec65ce6158a45647ac817
balteyes <julian.ivanova@gmail.com>
2024-09-27 16:26:57 +0300

    собрала орехи

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        Tree tree = new Tree();
+        Nut[] nuts = tree.growMassOfNuts (15);
+
         Squirrel bestSquirrel = new Squirrel();
-        bestSquirrel.collectNuts();
+
+        bestSquirrel.collectNuts(nuts);
 
 
 
--- src/Squirrel.java
+++ src/Squirrel.java
@@ -1,12 +1,12 @@
 public class Squirrel {
-    public void collectNuts(){
+    public void collectNuts(Nut[] nuts){
         double totalWeight = 0;
         for (Nut nut : nuts) {
             System.out.println("ура, еще орех!");
             totalWeight += nut.weight;
 
         }
-        System.out.println("Я собрала орехов на: " +totalWeight +" кг!");
+        System.out.println("Я собрала орехов на: " +totalWeight +" gr!");
 
         }
 
